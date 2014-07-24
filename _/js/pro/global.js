f626bbb44f4c09d12234d2d366fd8e09213f339c
Mike <mikeahurwitz@gmail.com>
2014-07-23 22:27:34 -0400

    Went back to old plain js, starting to animate triangle

--- _/js/pro/global.js
+++ _/js/pro/global.js
@@ -1,139 +1,5 @@
 (function($) {
-    var canvas;
 
-    var can = {
-        init: function(){
-            var $canvasParentCell;
-            
-            // Set up canvas object
-            canvas = new fabric.Canvas('canvas');
-
-            // Set canvas width/height
-            $canvasParentCell = $('#canvas').parents('.cell');
-            canvas.setWidth($canvasParentCell.width());
-            canvas.setHeight($canvasParentCell.height());
-
-            // Draw background
-            background.draw();
-
-            // Initialize triangle
-            triangle.init();
-
-            // Animate canvas
-            can.animate();
-        },
-        animate: function(){
-            // Animate Triangle
-            //triangle.draw();
-
-            // Request animation frame from browser to animate canvas
-            fabric.util.requestAnimFrame(can.animate, canvas.getElement());
-            canvas.renderAll();
-        },
-    }
-    var triangle = {
-        init: function(){
-            // Set starting angle for rotating
-            triangle.angle = 0;
-
-            // Set triangle side length
-            triangle.sideLength = 100;
-
-            // Draw triangle shape for first time
-            triangle.shape = triangle.createTriangle();
-            canvas.add(triangle.shape);
-
-            triangle.shape.animate('angle', 1440, {
-              onChange: canvas.renderAll.bind(canvas),
-              duration: 10000,
-              //easing: fabric.util.ease.easeOutBounce
-            });
-        },
-        createTriangle: function(){
-            var a,
-                h,
-                triCoords,
-                centerY,
-                pos,
-                shape;
-
-            // Get triangle height and width
-            a = triangle.sideLength;
-            h = a * (Math.sqrt(3)/2);
-
-            // Get x/y vars
-            x = canvas.width/2 - a/2;
-            y = canvas.height/2 - h/2;
-
-            // Move points up so triangle is center based on the triangle center
-            // not the outlying box center
-            triCoords = [
-                {x:0,y:-h/2},
-                {x:-a / 2,y:h/2},
-                {x:a / 2,y:h/2},
-            ]
-            centerY = (triCoords[0].y + triCoords[1].y + triCoords[2].y) / 3;
-            y -= centerY;
-
-            // Rotate points around center
-            pos = fabric.util.rotatePoint(
-                new fabric.Point(x, y),
-                new fabric.Point(x + a / 2, y + h / 3 * 2),
-                fabric.util.degreesToRadians(triangle.angle)
-            );
-            
-            // Create shape object and return it
-            shape =  new fabric.Triangle(
-            {
-                width: 100,
-                height: 100,
-                selectable: false,
-                fill: 'white',
-                strokeWidth: 2,
-                left: canvas.width/2,
-                top: canvas.height/2,
-                originX: 'center',
-                originY: 'center',
-                angle: triangle.angle,
-            });
-            return shape;
-        },
-        draw: function(){
-            canvas.renderAll(); 
-            
-            // Rotate Triangle
-            triangle.angle += 1;
-            canvas.remove(triangle.shape);
-            triangle.shape = triangle.createTriangle();
-            canvas.add(triangle.shape);    
-        },
-
-    }
-    var background = {
-        draw: function(){
-            // Add full width/height black background
-            var rect = new fabric.Rect({
-                left: 0,
-                top: 0,
-                fill: 'black',
-                width: canvas.width,
-                height: canvas.height,
-                selectable: false,
-            });
-            canvas.add(rect);
-        }
-    }
-
-
-
-    can.init();
-
-
-})(jQuery);
-/*
-
-
-(function($) {
 	// RequestAnimFrame: a browser API for getting smooth animations
     var countAniFrame = 0;
     var requestAnimFrame = (function(){
@@ -158,6 +24,8 @@
 
             // Initialize particles
            	particles.init();
+
+            triangle.init();
 
             // Kick off animation loop!
             canvas.animloop();
@@ -205,10 +73,48 @@
             canvas.ctx.restore();
 		},
 	}
+
+    function animate(opts) {
+            //var opts.start = 10;
+        //var id = setInterval(function() {
+            var timePassed = countAniFrame - 10
+            var progress = timePassed / opts.duration
+
+            if (progress > 1) progress = 1
+                var delta = opts.delta(progress)
+            if (progress != 1)
+                //requestAnimFrame(opts)
+            
+            opts.step(delta)
+
+            //console.log(countAniFrame)
+        
+        //        clearInterval(id)
+            //}
+        //}, opts.delay || 10)
+    }
     var triangle = {
         side: 150,
         count: 0,
         rotate: 0,
+        opacity: 0,
+        scale: 0,
+        animations: [
+            {
+                start: countAniFrame,
+                //delay: 0,
+                duration: 50,
+                delta: function(p) {return p},
+                step: function(delta) {
+                    triangle.rotate = 720*delta * Math.PI/180;
+                    triangle.opacity = 1 * delta;
+                    triangle.scale = 1 * delta;
+                }
+            }
+        ],
+        init: function(){
+
+        },
         draw: function(){
             var h,
                 triCoords,
@@ -228,9 +134,10 @@
             });
 
             canvas.ctx.save();
-            canvas.ctx.fillStyle = 'rgba(255,255,255,1)';
+            canvas.ctx.fillStyle = 'rgba(255,255,255,'+triangle.opacity+')';
             canvas.ctx.translate(canvas.can.width/2, canvas.can.height/2);
             canvas.ctx.rotate(triangle.rotate);
+            canvas.ctx.scale(triangle.scale, triangle.scale);
 
             canvas.ctx.beginPath();
             canvas.ctx.moveTo(triCoords[0].x, triCoords[0].y);
@@ -244,14 +151,28 @@
         },
         update: function(){
             //if (triangle.count < 300){
-                triangle.rotate = (triangle.count+1) * Math.PI/180;
+                //triangle.rotate = (triangle.count+1) * Math.PI/180;
             //}
+            // Animations!
+            triangle.animations.forEach(function(el){
+                var timePassed, 
+                    progress;
+                timePassed = countAniFrame - el.start;
+                if (el.delay)
+                    timePassed -= el.delay;
+                progress = timePassed / el.duration;
+                if (progress > 1) progress = 1;
+                if (progress <= 1 && progress >= 0){
+                    var delta = el.delta(progress)
+                    el.step(delta)
+
+                }
+
+
+            });
 
             triangle.count++;
         },
-    }
-    function animateValue(startFrame, endFrame, startValue, endValue){
-
     }
 	var particles = {
         //particleCount: 100,
@@ -348,4 +269,3 @@
 
 
 })(jQuery);
-*/
