e0b8bff2e84adbeeb12cffa3833698f45f125f63
Mike <mikeahurwitz@gmail.com>
2014-09-18 18:11:25 -0400

    Updating work devices

--- work.php
+++ work.php
@@ -52,50 +52,28 @@
 				<div class="tac">
 
 					<?php foreach ($work['images'] as $image) : ?>
-
-						<?php if ( $image['size'] == 'desktop' ) : ?>
-						
-							<div class="device device-desktop">
-								<div class="device-desktop-top">
-									<div class="device-desktop-buttons"></div>
-								</div>
-								<?php if ( isset($image['video']) ) : ?>
-									<video class="device-desktop-image" loop="true" autoplay="autoplay" poster="<?php echo $image['video']['poster']; ?>">
-										<?php if ( isset($image['video']['webm']) ) : ?><source type="video/webm" src="<?php echo $image['video']['webm']; ?>"><?php endif; ?>
-										<?php if ( isset($image['video']['ogv']) ) : ?><source type="video/ogg" src="<?php echo $image['video']['ogv']; ?>"><?php endif; ?>
-										<?php if ( isset($image['video']['mp4']) ) : ?><source type="video/mp4" src="<?php echo $image['video']['mp4']; ?>"><?php endif; ?>
-									</video>
-								<?php else : ?>
-									<img src="<?php echo $image['image']; ?>" alt="" class="device-desktop-image">
-								<?php endif; ?>
-							</div>
-
-						<?php elseif ( $image['size'] == 'phone' || $image['size'] == 'tablet' ) : ?>
-
-							<div class="device device-mobile device-mobile--<?php echo $image['size']; ?>">
-								<div class="device-mobile-circle device-mobile-circle--<?php echo $image['size']; ?>1"></div>
-								<div class="device-mobile-circle device-mobile-circle--<?php echo $image['size']; ?>2"></div>
-								<div class="device-mobile-home device-mobile-home--<?php echo $image['size']; ?>"></div>
-
-								<?php if ( $image['size'] == 'phone' ) : ?>
-									<div class="device-mobile-circle device-mobile-circle--phone3"></div>
-									<div class="device-mobile-speaker"></div>
-								<?php endif; ?>
-								
-								<div class="device-mobile-padding--<?php echo $image['size']; ?>">
-									<?php if ( isset($image['video']) ) : ?>
-										<video class="device-desktop-image" loop="true" autoplay="autoplay" poster="<?php echo $image['video']['poster']; ?>">
-											<?php if ( isset($image['video']['webm']) ) : ?><source type="video/webm" src="<?php echo $image['video']['webm']; ?>"><?php endif; ?>
-											<?php if ( isset($image['video']['ogv']) ) : ?><source type="video/ogg" src="<?php echo $image['video']['ogv']; ?>"><?php endif; ?>
-											<?php if ( isset($image['video']['mp4']) ) : ?><source type="video/mp4" src="<?php echo $image['video']['mp4']; ?>"><?php endif; ?>
-										</video>
-									<?php else : ?>
-										<img src="<?php echo $image['image']; ?>" alt="" class="device-mobile-image">
-									<?php endif; ?>
-								</div>
-							</div>
-
-						<?php endif; ?>
+						<div class="device device--<?php echo $image['size'] ?>">
+							
+							<?php if ( isset($image['video']) ) : ?>
+							
+								<video class="device-image" loop="true" autoplay="autoplay" poster="<?php echo $image['video']['poster']; ?>">
+									<?php if ( isset($image['video']['webm']) ) : ?><source type="video/webm" src="<?php echo $image['video']['webm']; ?>"><?php endif; ?>
+									<?php if ( isset($image['video']['ogv']) ) : ?><source type="video/ogg" src="<?php echo $image['video']['ogv']; ?>"><?php endif; ?>
+									<?php if ( isset($image['video']['mp4']) ) : ?><source type="video/mp4" src="<?php echo $image['video']['mp4']; ?>"><?php endif; ?>
+								</video>
+							
+							<?php else : ?>
+							
+								<img src="<?php echo $image['image']; ?>" alt="" class="device-image">
+							
+							<?php endif; ?>
+							
+							<?php if ( $image['size'] == 'desktop' ) : ?>
+							
+								<div class="device-buttons"></div>
+							
+							<?php endif; ?>
+						</div>
 
 					<?php endforeach;?>
 
