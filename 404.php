f3e8fa3e9e4c62936b8afa5d0604b542582f3963
Mike <mikeahurwitz@gmail.com>
2014-09-21 14:37:48 -0400

    Fixing 404 so it shows on all screens

--- 404.php
+++ 404.php
@@ -2,18 +2,18 @@
 <?php require('nav.php'); ?>
 
 
-<section class="row row--window">
-	<div class="cell cell--half cell--color bc1 pos ofh">
-		<h1 class="h1">404</h1>
+<section class="row row--minwindow">
+	<div class="cell cell--half cell--color bc1 poa ofh">
+		<div class="well">
+			<h1 class="h1">404</h1>
+		</div>
 	</div>
-	<div class="cell cell--half cell--mono pos ofh">
+	<div class="cell cell--half cell--mono cell--404">
+		<div class="well">
 		<h1 class="h1 h1--right">404</h1>
-		<div class="table">
-			<div class="table-cell table-cell--middle">
-				<div class="cell cell--s cell-content--left ">
-					<p>The page you're looking for can't be found. Maybe visiting the <a href="<?php echo $page_base; ?>">homepage</a> will help.</p>
-				</div>
-			</div>
+		<div class="cell cell--s cell-content--left fn">
+			<p>The page you're looking for can't be found. Maybe visiting the <a href="<?php echo $page_base; ?>">homepage</a> will help.</p>
+		</div>
 		</div>
 	</div>
 </section>
